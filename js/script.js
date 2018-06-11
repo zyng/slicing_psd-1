9fb9c2a985244d1286656ce4aa4ac52fc95d8e37
zyng <kisielewskimichal@gmail.com>
2018-06-11 22:27:50 +0200

    adding responsiveness

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 $('.menu-toggle').click(function () {
     'use strict';
     $(this).toggleClass('open');
-    $('.menu-nav ul').toggleClass('menu-items-col');
-    $('.part2').toggle();
+//    $('.menu-nav ul').toggleClass('menu-items-col');
+//    $('.part2').toggle();
 
 });
 
 
-$(window).resize(function () {
-    'use strict';
-    var viewportWidth = $(window).width();
-
-    if ($('.menu-nav ul').hasClass('menu-items-col') && viewportWidth > 840) {
-        $('.menu-nav ul').removeClass("menu-items-col");
-        $('.menu-toggle').removeClass('open');
-        $('.part2').show();
-    } else if (viewportWidth > 840) {
-        $('.part2').show();
-        $('.menu-toggle').removeClass('open');
-    } else if (viewportWidth <= 840 && !$('.menu-nav ul').hasClass('menu-items-col')) {
-        $('.part2').hide();
-    }
-});
+//$(window).resize(function () {
+//    'use strict';
+//    var viewportWidth = $(window).width();
+//
+//    if ($('.menu-nav ul').hasClass('menu-items-col') && viewportWidth > 840) {
+//        $('.menu-nav ul').removeClass("menu-items-col");
+//        $('.menu-toggle').removeClass('open');
+//        $('.part2').show();
+//    } else if (viewportWidth > 840) {
+//        $('.part2').show();
+//        $('.menu-toggle').removeClass('open');
+//    } else if (viewportWidth <= 840 && !$('.menu-nav ul').hasClass('menu-items-col')) {
+//        $('.part2').hide();
+//    }
+//});
