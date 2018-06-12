a8d9f87b29a20f35e2832acdcb922bdec1b0d64a
zyng <kisielewskimichal@gmail.com>
2018-06-12 10:37:23 +0200

    conversion  jquery code to vanilla js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-$('.container-header-navigation').click(function () {
-    'use strict';
-    $(this).toggleClass('open');
-//    $('.menu-nav ul').toggleClass('menu-items-col');
-//    $('.part2').toggle();
+var hamburger = document.querySelector('.menu-toggle')
 
-});
+hamburger.addEventListener("click", function () {
+    document.querySelector(".container-header-navigation").classList.toggle("open");
+}, false);
 
-
-//var hamburger = document.querySelector(".hamburger");
-//hamburger.addEventListener("click", function () {
-//  document.querySelector(".page-header").classList.toggle("nav-opened");
-//}, false);
 
 $(window).resize(function () {
 
