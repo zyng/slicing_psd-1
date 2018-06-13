0dfec8576eaee78d96df4fb42142a58c35d8d828
zyng <kisielewskimichal@gmail.com>
2018-06-13 14:56:54 +0200

    change for sticky menu

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
-var hamburger = document.querySelector('.menu-toggle')
-
-hamburger.addEventListener("click", function () {
-    document.querySelector(".container-header-navigation").classList.toggle("open");
-}, false);
-
-
-$(window).resize(function () {
-
-
-    var viewportWidth = $(window).width();
-
-    if (viewportWidth >= 840) {
-        $('.container-header-navigation').removeClass('open');
-    }
-});
-
-
 $(document).ready(function() {
 
-  var scrollTo = function (link, goto ,speed) {
+    var hamburger = document.querySelector('.menu-toggle')
 
-    $(link).click(function(){
-      $('html,body').animate({
-        scrollTop: $(goto).offset().top
-      },speed);
+    hamburger.addEventListener("click", function () {
+        document.querySelector(".container-header-navigation").classList.toggle("open");
+    }, false);
+
+
+    $(window).resize(function () {
+        var viewportWidth = $(window).width();
+
+        if (viewportWidth >= 840) {
+            $('.container-header-navigation').removeClass('open');
+        }
     });
-  }
 
-  scrollTo("#link1", "#section-home", 1500);
-  scrollTo("#link2", "#section-about", 1500);
-  scrollTo("#link3", "#section-services", 1500);
-  scrollTo("#link4", "#section-gallery", 1500);
-  scrollTo("#link5", "#section-blog", 1500);
-  scrollTo("#link6", "#section-contact", 1500);
+
+    var scrollTo = function (link, goto ,speed) {
+
+        $(link).click(function(){
+            $('html,body').animate({
+            scrollTop: $(goto).offset().top
+        },speed)});
+    }
+
+    scrollTo("#link1", "#section-home", 1500);
+    scrollTo("#link2", "#section-about", 1500);
+    scrollTo("#link3", "#section-services", 1500);
+    scrollTo("#link4", "#section-gallery", 1500);
+    scrollTo("#link5", "#section-blog", 1500);
+    scrollTo("#link6", "#section-contact", 1500);
 
     var NavY = $('.page-main').offset().top;
 
     var stickyNav = function () {
-       ScrollY = $(window).scrollTop();
+        ScrollY = $(window).scrollTop();
 
 
         if (ScrollY > NavY) {
             $('.part2').addClass('sticky');
-        } else {
+        }
+        else {
             $('.part2').removeClass('sticky');
         }
 
@@ -50,50 +48,60 @@ $(document).ready(function() {
 
 	stickyNav();
 
+
     var about = $('.section-main-about').offset().top;
     var services = $('.section-main-services').offset().top;
     var gallery = $('.section-main-gallery').offset().top;
     var blog = $('.section-main-blog').offset().top;
     var contact = $('.page-footer').offset().top;
+    var currentvalue = $('.sticky').css('maxWidth');
 
-     var change_background_size = function () {
+
+    var sizeChange = function () {
+
         var ScrollY = $(window).scrollTop();
+        var sticky = $('.sticky');
 
+        if (currentvalue != "135px" && ScrollY >= about && ScrollY < services) {
+            sticky.animate({maxWidth:"135px"},800);
+            currentvalue = "135px";
+        }
+        else if (currentvalue != "230px" && ScrollY >= services && ScrollY < gallery) {
+            sticky.animate({maxWidth:"230px"},800);
+            currentvalue = "230px";
+        }
+        else if (currentvalue != "320px" && ScrollY >= gallery && ScrollY < blog) {
+            sticky.animate({maxWidth:"320px"},800);
+            currentvalue = "320px";
+        }
+        else if (currentvalue != "385px" && ScrollY >= blog && ScrollY < contact) {
+            sticky.animate({maxWidth:"385px"},800);
+            currentvalue = "385px";
+        }
+        else if (currentvalue != "480px" && ScrollY >= contact) {
+            sticky.animate({maxWidth:"480px"},800);
+            currentvalue = "480px";
+        }
+        else if (currentvalue != '' && ScrollY < about) {
+            $('.part2').css('maxWidth', '');
+            currentvalue = '';
+        }
 
-        if (ScrollY >= about && ScrollY < services) {
-            $('.sticky').css('maxWidth',"135px");
-        }
-        else if (ScrollY >= services && ScrollY < gallery) {
-            $('.sticky').css('maxWidth',"230px");
-        }
-        else if (ScrollY >= gallery && ScrollY < blog) {
-            $('.sticky').css('maxWidth',"320px");
-        }
-        else if (ScrollY >= blog && ScrollY < contact) {
-            $('.sticky').css('maxWidth',"385px");
-        }
-        else if (ScrollY >= contact) {
-            $('.sticky').css('maxWidth',"480px");
-        }
-        else if (ScrollY < about) {
-            $('.sticky').css('maxWidth',"");
-        }
 
         if (ScrollY >= gallery && ScrollY < blog) {
-            $('.sticky').css('background-color', 'rgba(255,255,255,0.9)');
-        }else {
-            $('.sticky').css('background-color', '');
+            sticky.css('background-color', 'rgba(255,255,255,0.9)');
+        }
+        else {
+            sticky.css('background-color', '');
         }
 
+    }
 
-    };
 
-    change_background_size();
-
-	$(window).scroll(function () {
-        change_background_size();
+    $(window).scroll( function () {
         stickyNav();
-
-	});
+        sizeChange();
+    });
 
 });
+
